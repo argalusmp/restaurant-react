fb2758057e5c942bda35c63825d99f32dc6cac1a
argalusmp <argalus03@gmail.com>
2023-07-01 23:14:30 +0700

    create about

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import { SubHeading } from '../../components';
+import { images } from "../../constants";
+
 
 import './Header.css';
 
+
 const Header = () => (
-  <div>
-    Header
+  <div className='app__header app__wrapper section__padding' id='home'>
+    <div className='app__wrapper_info'>
+      <SubHeading title ="Chase the new flavour" />
+      <h1 className='app__header-h1'> The Key to Fine Dining</h1>  
+      <p className='p__opensans' style={{margin:'2rem 0'}} >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <button type='button' className='custom__button'>Explore Menu</button>
+    </div>
+
+    <div className='app__wrapper_img'>
+      <img src={images.welcome} alt='header image'></img>
+    </div>
   </div>
 );
 
